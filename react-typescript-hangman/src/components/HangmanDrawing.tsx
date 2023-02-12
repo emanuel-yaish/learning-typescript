867d9d7a0bbe676ce3428ee79ec7f42076a7f3c2
Emanuel Yaish <emanuelyaish@gmail.com>
2023-02-12 23:20:40 +0200

    add hangman draw component

--- react-typescript-hangman/src/components/HangmanDrawing.tsx
+++ react-typescript-hangman/src/components/HangmanDrawing.tsx
@@ -1,5 +1,19 @@
 import React from "react";
+import "./HangmanDrawing.css";
 
 export const HangmanDrawing = () => {
-  return <div>HangmanDrawing</div>;
+  return (
+    <div className="hangman-drawing">
+      <div className="hangman-drawing-head"></div>
+      <div className="hangman-drawing-body"></div>
+      <div className="hangman-drawing-left-arm"></div>
+      <div className="hangman-drawing-right-arm"></div>
+      <div className="hangman-drawing-left-leg"></div>
+      <div className="hangman-drawing-right-leg"></div>
+      <div className="hangman-drawing-top"></div>
+      <div className="hangman-drawing-rope"></div>
+      <div className="hangman-drawing-pole"></div>
+      <div className="hangman-drawing-bottom"></div>
+    </div>
+  );
 };
